0478c4df767f59546b3b837c4db339702fb0f20b
Benjamin Canac <canacb1@gmail.com>
2024-01-31 16:16:20 +0100

    chore(app): tooltip open delay

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,11 @@
 export default defineAppConfig({
   ui: {
     primary: 'sky',
-    gray: 'cool'
+    gray: 'cool',
+    tooltip: {
+      default: {
+        openDelay: 500
+      }
+    }
   }
 })
