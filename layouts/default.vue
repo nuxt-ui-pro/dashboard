911485dfb04fde01652cd23cf36237544f0b199b
Benjamin Canac <canacb1@gmail.com>
2024-02-13 18:10:06 +0100

    chore(default): add `View page source` command

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+const route = useRoute()
 const appConfig = useAppConfig()
 const { isHelpSlideoverOpen } = useDashboard()
 
@@ -62,6 +63,15 @@ const footerLinks = [{
   click: () => isHelpSlideoverOpen.value = true
 }]
 
+const commands = [{
+  id: 'source',
+  label: 'View page source',
+  icon: 'i-simple-icons-github',
+  click: () => {
+    window.open(`https://github.com/nuxt-ui-pro/dashboard/blob/main/pages${route.path === '/' ? '/index' : route.path}.vue`, '_blank')
+  }
+}]
+
 const defaultColors = ref(['green', 'teal', 'cyan', 'sky', 'blue', 'indigo', 'violet'].map(color => ({ label: color, chip: color, click: () => appConfig.ui.primary = color })))
 const colors = computed(() => defaultColors.value.map(color => ({ ...color, active: appConfig.ui.primary === color.label })))
 </script>
@@ -77,7 +87,7 @@ const colors = computed(() => defaultColors.value.map(color => ({ ...color, acti
 
       <UDashboardSidebar>
         <template #header>
-          <UDashboardSearchButton color="white" />
+          <UDashboardSearchButton />
         </template>
 
         <UDashboardSidebarLinks :links="links" />
@@ -107,7 +117,7 @@ const colors = computed(() => defaultColors.value.map(color => ({ ...color, acti
     <NotificationsSlideover />
 
     <ClientOnly>
-      <LazyUDashboardSearch :groups="[{ key: 'links', label: 'Go to', commands: links }]" />
+      <LazyUDashboardSearch :groups="[{ key: 'links', label: 'Go to', commands: links }, { key: 'code', label: 'Code', commands }]" />
     </ClientOnly>
   </UDashboardLayout>
 </template>
