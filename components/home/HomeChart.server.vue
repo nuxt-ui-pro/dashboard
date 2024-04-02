f9edd73e6da1445da9d003016928a52a4db16392
Sébastien Chopin <seb@nuxt.com>
2024-04-03 01:55:49 +0200

    fix: userAgent error on CF

---
+++ components/home/HomeChart.server.vue
@@ -0,0 +1,14 @@
+<template>
+  <UDashboardCard ref="cardRef" :ui="{ body: { padding: '!pb-3 !px-0' } }">
+    <template #header>
+      <div>
+        <p class="text-sm text-gray-500 dark:text-gray-400 font-medium mb-1">
+          Revenue
+        </p>
+        <p class="text-3xl text-gray-900 dark:text-white font-semibold">
+          ---
+        </p>
+      </div>
+    </template>
+  </UDashboardCard>
+</template>
