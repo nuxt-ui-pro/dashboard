edb8103cd85b86d7403000b4bafd27a8271bf3e5
Benjamin Canac <canacb1@gmail.com>
2025-02-17 16:00:31 +0100

    up

--- app/error.vue
+++ app/error.vue
@@ -1,16 +1,13 @@
 <script setup lang="ts">
 import type { NuxtError } from '#app'
 
+defineProps<{
+  error: NuxtError
+}>()
+
 useSeoMeta({
   title: 'Page not found',
   description: 'We are sorry but this page could not be found.'
-})
-
-defineProps({
-  error: {
-    type: Object as PropType<NuxtError>,
-    required: true
-  }
 })
 
 useHead({
