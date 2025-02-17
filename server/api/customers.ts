1036081faf62785d90dd828b822ed4e7724fecec
HugoRCD <hugo.richard@epitech.eu>
2025-02-17 17:21:44 +0100

    up

--- server/api/customers.ts
+++ server/api/customers.ts
@@ -1,4 +1,4 @@
-import type { User, UserStatus } from '~/types'
+import type { User } from '~/types'
 
 const customers: User[] = [{
   id: 1,
@@ -182,31 +182,6 @@ const customers: User[] = [{
   location: 'London, UK'
 }]
 
-export default eventHandler(async (event) => {
-  // const { q, statuses, locations, sort, order } = getQuery(event) as { q?: string, statuses?: UserStatus[], locations?: string[], sort?: 'name' | 'email', order?: 'asc' | 'desc' }
-
+export default eventHandler(async () => {
   return customers
-
-  /* return customers.filter((user) => {
-    if (!q) return true
-
-    return user.name.search(new RegExp(q, 'i')) !== -1 || user.email.search(new RegExp(q, 'i')) !== -1
-  }).filter((user) => {
-    if (!statuses?.length) return true
-
-    return statuses.includes(user.status)
-  }).filter((user) => {
-    if (!locations?.length) return true
-
-    return locations.includes(user.location)
-  }).sort((a, b) => {
-    if (!sort) return 0
-
-    const aValue = a[sort]
-    const bValue = b[sort]
-
-    if (aValue < bValue) return order === 'asc' ? -1 : 1
-    if (aValue > bValue) return order === 'asc' ? 1 : -1
-    return 0
-  }) */
 })
