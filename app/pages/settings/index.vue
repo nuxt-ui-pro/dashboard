35ebc7cb1e81700c1c6c685a3ad62096f37c1ef8
HugoRCD <hugo.richard@epitech.eu>
2025-02-18 15:12:54 +0100

    up

--- app/pages/settings/index.vue
+++ app/pages/settings/index.vue
@@ -36,7 +36,12 @@ const validate = (state: any): FormError[] => {
 
 const toast = useToast()
 async function onSubmit(event: FormSubmitEvent<Schema>) {
-  toast.add({ title: 'Success', description: 'The form has been submitted.', color: 'success' })
+  toast.add({
+    title: 'Success',
+    description: 'Your settings have been updated.',
+    icon: 'i-lucide-check',
+    color: 'success'
+  })
   console.log(event.data)
 }
 
@@ -65,68 +70,71 @@ function onFileClick() {
     <div class="flex justify-between items-center gap-4">
       <UFormField label="Profile" description="This information will be displayed publicly so be careful what you share." />
       <UButton
+        form="settings"
         label="Save changes"
         color="neutral"
+        type="submit"
       />
     </div>
     <USeparator />
     <UForm
+      id="settings"
+      :schema="schema"
       :state="state"
       :validate="validate"
       class="space-y-4"
       @submit="onSubmit"
     >
-      <div class="flex justify-between items-center gap-4">
-        <UFormField
-          name="name"
-          label="Name"
-          description="Will appear on receipts, invoices, and other communication."
-          required
-        />
+      <UFormField
+        name="name"
+        label="Name"
+        description="Will appear on receipts, invoices, and other communication."
+        required
+        class="flex justify-between items-start gap-4"
+      >
         <UInput
           v-model="state.name"
           autocomplete="off"
           size="md"
         />
-      </div>
+      </UFormField>
       <USeparator />
-      <div class="flex justify-between items-center gap-4">
-        <UFormField
-          name="email"
-          label="Email"
-          description="Used to sign in, for email receipts and product updates."
-          required
-        />
+      <UFormField
+        name="email"
+        label="Email"
+        description="Used to sign in, for email receipts and product updates."
+        required
+        class="flex justify-between items-start gap-4"
+      >
         <UInput
           v-model="state.email"
           type="email"
           autocomplete="off"
           size="md"
         />
-      </div>
+      </UFormField>
       <USeparator />
-      <div class="flex justify-between items-center gap-4">
-        <UFormField
-          name="username"
-          label="Username"
-          description="Your unique username for logging in and your profile URL."
-          required
-        />
+      <UFormField
+        name="username"
+        label="Username"
+        description="Your unique username for logging in and your profile URL."
+        required
+        class="flex justify-between items-start gap-4"
+      >
         <UInput
           v-model="state.username"
           type="username"
           autocomplete="off"
           size="md"
-          input-class="ps-[77px]"
         />
-      </div>
+      </UFormField>
       <USeparator />
-      <div class="flex justify-between items-center gap-4">
-        <UFormField
-          name="avatar"
-          label="Avatar"
-          help="JPG, GIF or PNG. 1MB Max."
-        />
+      <UFormField
+        name="avatar"
+        label="Avatar"
+        description="JPG, GIF or PNG. 1MB Max."
+        class="flex justify-between items-center gap-4"
+      >
         <div class="flex flex-wrap items-center gap-3">
           <UAvatar
             :src="state.avatar"
@@ -147,14 +155,15 @@ function onFileClick() {
             @change="onFileChange"
           >
         </div>
-      </div>
+      </UFormField>
       <USeparator />
-      <div class="flex justify-between items-center gap-4">
-        <UFormField
-          name="bio"
-          label="Bio"
-          description="Brief description for your profile. URLs are hyperlinked."
-        />
+      <UFormField
+        name="bio"
+        label="Bio"
+        description="Brief description for your profile. URLs are hyperlinked."
+        class="flex justify-between items-start gap-4"
+        :ui="{ container: 'w-full' }"
+      >
         <UTextarea
           v-model="state.bio"
           :rows="5"
@@ -162,46 +171,48 @@ function onFileClick() {
           class="w-full"
           size="md"
         />
-      </div>
+      </UFormField>
       <USeparator />
-      <div class="flex justify-between items-center gap-4">
-        <UFormField
-          name="password"
-          label="Password"
-          description="Confirm your current password before setting a new one."
-        />
-        <div class="flex flex-col gap-1">
-          <UInput
-            id="password"
-            v-model="state.password_current"
-            type="password"
-            placeholder="Current password"
-            size="md"
-          />
-          <UInput
-            id="password_new"
-            v-model="state.password_new"
-            type="password"
-            placeholder="New password"
-            size="md"
-            class="mt-2"
-          />
+      <UFormField
+        name="password"
+        label="Password"
+        description="Confirm your current password before setting a new one."
+        class="flex justify-between items-start gap-4"
+      >
+        <div class="flex flex-col gap-2">
+          <UFormField name="password_current">
+            <UInput
+              id="password"
+              v-model="state.password_current"
+              type="password"
+              placeholder="Current password"
+              size="md"
+            />
+          </UFormField>
+          <UFormField name="password_new">
+            <UInput
+              id="password_new"
+              v-model="state.password_new"
+              type="password"
+              placeholder="New password"
+              size="md"
+            />
+          </UFormField>
         </div>
-      </div>
+      </UFormField>
       <USeparator />
-      <div class="flex flex-col items-end gap-4">
-        <UFormField
-          name="Account"
-          label="Account"
-          description="No longer want to use our service? You can delete your account here. This action is not reversible. All information related to this account will be deleted permanently."
-          class="w-full"
-        />
+      <UFormField
+        name="Account"
+        label="Account"
+        description="No longer want to use our service? You can delete your account here. This action is not reversible. All information related to this account will be deleted permanently."
+        class="flex flex-col items-end gap-4"
+      >
         <UButton
           label="Delete account"
           color="error"
           size="md"
         />
-      </div>
+      </UFormField>
     </UForm>
   </div>
 </template>
